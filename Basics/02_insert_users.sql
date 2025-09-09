6d0dc3f762719cd8f7e7e8bade453593cad50b06
Swapnil-code-maker <itsswapnil0@gmail.com>
2025-09-09 23:39:35 +0530

    Create 02_insert_users.sql
    Add insert queries for users table

---
+++ Basics/02_insert_users.sql
@@ -0,0 +1,8 @@
+-- Insert sample data into users table
+INSERT INTO users (name, email, gender, date_of_birth, salary) 
+VALUES
+('Rahul Sharma', 'rahul@example.com', 'Male', '2000-05-10', 55000.00),
+('Ananya Gupta', 'ananya@example.com', 'Female', '1999-11-23', 60000.00),
+('Amit Verma', 'amit@example.com', 'Male', '2001-01-15', 45000.00),
+('Priya Mehta', 'priya@example.com', 'Female', '1998-08-30', 75000.00),
+('Rohan Singh', 'rohan@example.com', 'Male', '2002-02-05', 50000.00);
